6737e9ad63efb22af265ed7ffcd4d88188942c6c
MalekNakachi <malek.nakachi@esprit.tn>
2022-03-23 18:59:43 +0100

    Initial commit

---
+++ client/src/main/java/com/clientui/clinetui/proxies/MicroserviceProduitsProxy.java
@@ -0,0 +1,21 @@
+package com.clientui.clinetui.proxies;
+
+
+import com.clientui.clientui.com.clientui.clientui.beans.ProductBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+
+@FeignClient(name = "microservice-produits", url = "localhost:4545")
+public interface MicroserviceProduitsProxy {
+    @GetMapping(value = "/produits")
+    List<ProductBean> listeDesProduits();
+
+    @GetMapping( value = "/produits/{id}")
+    ProductBean recupererUnProduit(@PathVariable("id") int id);
+
+}
---
+++ microservice-produits/src/main/java/com/mproduits/web/configuration/ApplicationPropertiesConfiguration.java
@@ -0,0 +1,27 @@
+package com.mproduits.web.configuration;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//import org.springframework.stereotype.Component;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("mes-configs")
+public class ApplicationPropertiesConfiguration
+{
+
+    private int limitDeProduits;
+
+    public int getLimitDeProduits()
+    {
+
+        return limitDeProduits;
+
+    }
+
+    public void setLimitDeProduits(int limitDeProduits)
+    {
+        this.limitDeProduits = limitDeProduits;
+    }
+}
